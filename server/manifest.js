a85d4ba834cad2c9c9e6e58b53fd5cfaac65cc6b
george fisher <georgezappa@gmail.com>
2020-09-30 11:02:44 -0500

    Initialize flavor-docker

---
+++ server/manifest.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Confidence = require('confidence');
+const Toys = require('toys');
+
+// Glue manifest as a confidence store
+module.exports = new Confidence.Store({
+    server: {
+        host: '0.0.0.0',
+        port: {
+            $env: 'PORT',
+            $coerce: 'number',
+            $default: 3000
+        },
+        debug: {
+            $filter: { $env: 'NODE_ENV' },
+            $default: {
+                log: ['error'],
+                request: ['error']
+            },
+            production: {
+                request: ['implementation']
+            }
+        }
+    },
+    register: {
+        plugins: [
+            {
+                plugin: '../lib', // Main plugin
+                options: {}
+            },
+            {
+                plugin: {
+                    $filter: { $env: 'NODE_ENV' },
+                    $default: 'hpal-debug',
+                    production: Toys.noop
+                }
+            }
+        ]
+    }
+});
